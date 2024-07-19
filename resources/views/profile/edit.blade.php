b48cb5837ef3420b4ca021eee376e5eaabc67565
Michał Żuk <michal.zuk30601@gmail.com>
2024-07-19 12:45:42 +0200

    translate messages and labels to polish

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            {{ __('Mój profil') }}
         </h2>
     </x-slot>
 
